6ed56884a5807555a81a4ea474c835e9323fd990
guozhaokui <guozhaokuiwang@gmail.com>
2016-07-17 22:10:19 +0800

    driver test

---
+++ VDriver/gamepad.ts
@@ -0,0 +1,64 @@
+/**
+ * https://developer.mozilla.org/en-US/docs/Web/API/Gamepad_API/Using_the_Gamepad_API
+ */
+
+/**
+ * connect事件后， 可以通过 navigator.getGamepads() 来得到一个数组
+ */
+
+var gamepads:Gamepad = null;
+
+function gamepadHandler(event:GamepadEvent, connecting) {
+  var gamepad = event.gamepad;
+  // Note:
+  // gamepad === navigator.getGamepads()[gamepad.index]
+
+  if (connecting) {
+    gamepads[gamepad.index] = gamepad;
+  } else {
+    delete gamepads[gamepad.index];
+  }
+}
+
+window.addEventListener("gamepadconnected", function(e:GamepadEvent) { gamepadHandler(e, true); }, false);
+window.addEventListener("gamepaddisconnected", function(e:GamepadEvent) { gamepadHandler(e, false); }, false);
+
+var controllers = {};
+function updateStatus() {
+  var i = 0;
+  var j;
+
+  for (j in controllers) {
+    var controller = controllers[j];
+    var d = document.getElementById("controller" + j);
+    var buttons = d.getElementsByClassName("button");
+
+    for (i = 0; i < controller.buttons.length; i++) {
+      var b = buttons[i];
+      var val = controller.buttons[i];
+      var pressed = val == 1.0;
+      if (typeof(val) == "object") {
+        pressed = val.pressed;
+        val = val.value;
+      }
+
+      var pct = Math.round(val * 100) + "%";
+      b.style.backgroundSize = pct + " " + pct;
+
+      if (pressed) {
+        b.className = "button pressed";
+      } else {
+        b.className = "button";
+      }
+    }
+
+    var axes = d.getElementsByClassName("axis");
+    for (i = 0; i < controller.axes.length; i++) {
+      var a = axes[i];
+      a.innerHTML = i + ": " + controller.axes[i].toFixed(4);
+      a.setAttribute("value", controller.axes[i] + 1);
+    }
+  }
+
+  requestAnimationFrame(updateStatus);
+}
